2abd1ff40aafb0ef65a2cb15baa07ec8a7d4b3e0
tongcixin <1404875284@qq.com>
2023-03-27 09:06:57 +0800

    change

--- src/main/java/com/wl/request/ResultServlet.java
+++ src/main/java/com/wl/request/ResultServlet.java
@@ -1,14 +1,23 @@
 package com.wl.request;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "ResultServlet", value = "/ResultServlet")
+@WebServlet(name = "ResultServlet", urlPatterns = "/ResultServlet")
 public class ResultServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        String username = (String) request.getAttribute("username");
+        if (username != null) {
+            out.println("用户名：" + username + "<br/>");
+        }
 
     }
 
